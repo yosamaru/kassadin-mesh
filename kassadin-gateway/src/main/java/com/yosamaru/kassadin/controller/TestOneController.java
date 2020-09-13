3d6bb41511086c0d7424a596656e319c981b8f67
Lei.W <lei@LeiWdeMacBook-Pro.local>
2020-09-13 16:01:36 +0800

    feat<网关路由>添加网关路由

--- kassadin-gateway/src/main/java/com/yosamaru/kassadin/controller/TestOneController.java
+++ kassadin-gateway/src/main/java/com/yosamaru/kassadin/controller/TestOneController.java
@@ -1,15 +1,21 @@
 package com.yosamaru.kassadin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/test")
 public class TestOneController {
+    @Value("${aaa:}")
+    private String a1;
+
     @RequestMapping("/1")
     @ResponseBody
-    public String test01(){
-        return "test01";
+    public String test01() {
+        return a1;
     }
 }
--- kassadin-search/src/test/java/com/yosamaru/kassadin/search/KassadinSearchApplicationTests.java
+++ kassadin-search/src/test/java/com/yosamaru/kassadin/search/KassadinSearchApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
-import org.springframework.data.elasticsearch.core.IndexOperations;
+//import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -33,10 +33,10 @@ public class KassadinSearchApplicationTests {
 
     @Test
     public void testEsProductMapping() {
-        IndexOperations indexOperations = elasticsearchTemplate.indexOps(EsProduct.class);
-        indexOperations.putMapping(indexOperations.createMapping(EsProduct.class));
-        Map mapping = indexOperations.getMapping();
-        System.out.println(mapping);
+//        IndexOperations indexOperations = elasticsearchTemplate.indexOps(EsProduct.class);
+//        indexOperations.putMapping(indexOperations.createMapping(EsProduct.class));
+//        Map mapping = indexOperations.getMapping();
+//        System.out.println(mapping);
     }
 
 }
